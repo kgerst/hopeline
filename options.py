a79064dea7f3a04af0e79ef9384d367790d9a287
Dana K. Engebretson <dkengebretson@spscommerce.com>
2017-01-14 17:27:06 -0600

    first attempt at breaking out the decision tree of asking questions!

---
+++ options.py
@@ -0,0 +1,47 @@
+def ask_feeling():
+    return {
+        'id': 1,
+        'message': "Are you planning to harm yourself? Reply Yes or No"
+    }
+
+def suggest_911():
+    return {
+        'id': 2,
+        'message': "Please call 911."
+    }
+
+def ask_to_reach_out():
+	return {
+        'id': 3,
+        'message': "Have you talked to someone about this feeling? Reply Yes or No"
+    }
+
+def ask_willingness():
+	return {
+        'id': 4,
+        'message': "Are you willing to? Reply Yes or No"
+    }
+
+def ask_type_of_contact():
+	return {
+        'id': 5,
+        'message': "Do you want help connecting with a Professional or reaching out to someone you have a personal connection to? Reply Professional or Personal"
+    }
+
+def give_resources():
+	return {
+        'id': 6,
+        'message': "Here are some resources... TODO"
+    }
+
+def suggest_how_to_start_convo():
+	return {
+        'id': 7,
+        'message': "DEARMAN How to"
+    }
+
+def suggest_skills():
+	return {
+        'id': 8,
+        'message': "Want to try a skill to help yourself? Reply Yes or No"
+    }
