d8df7f0f2300cd74073cdbdaab3e8d8d23944773
Dana K. Engebretson <dkengebretson@spscommerce.com>
2017-01-14 17:52:36 -0600

    including new options decision tree

--- decision.py
+++ decision.py
@@ -1,18 +1,18 @@
-def next(hopeline_id, answer):
+import options
+
+def next(hopeline_id, answer): 
+    options_dict = {
+        (1, 'Yes'): options.suggest_911(),
+        (1, 'No'): options.ask_to_reach_out(),
+        (3, 'Yes'): options.suggest_skills(),
+        (3, 'No'): options.ask_willingness(),
+        (4, 'Yes'): options.ask_type_of_contact(),
+        (4, 'No'): options.suggest_skills(),
+        (5, 'Professional'): options.give_resources(),
+        (5, 'Personal'): options.suggest_how_to_start_convo()
+    }
     if hopeline_id == 0:
-        return {
-            'id': 1,
-            'message': 'What\'s your name?'
-        }
-    elif hopeline_id == 1:
-        return {
-            'id': 2,
-            'message': 'Hello, %s!' % answer
-        }
+        options.ask_feeling()
     else:
-        print(hopeline_id)
-        hopeline_id += 1
-        return {
-            'id': hopeline_id,
-            'message': 'new state: %s' % hopeline_id
-        }
+        options_dict[(hopeline_id, answer)]
+
